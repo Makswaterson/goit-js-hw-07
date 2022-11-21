314c84d53658d0d5b7e9f73951c1696ebbec4d30
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-21 23:53:11 +0200

    add 7hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join();
+
+const onGalleryClick = evt => {
+  evt.preventDefault();
+};
+
+galleryRef.insertAdjacentHTML('beforebegin', galleryMarkup);
+galleryRef.addEventListener('click', onGalleryClick);
